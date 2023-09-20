2881c3d26d5728faddaca420579ab14277bcb30c
MBaroky <m.baroky@gmail.com>
2023-09-20 12:26:40 +0200

    home page almost over

---
+++ assets/js/Simple-Slider.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Initializing the swiper plugin for the slider.
+    // Read more here: http://idangero.us/swiper/api/
+    
+    // var mySwiper = new Swiper('.swiper-container', {
+    //     loop: true,
+    //     direction:"vertical",
+    //     navigation: {
+    //         nextEl: '.swiper-button-next',
+    //         prevEl: '.swiper-button-prev'
+    //     }
+    // });
+    const swiper = new Swiper('.swiper', {
+      speed: 400,
+      spaceBetween: 100,
+      direction:'vertical',
+      autoplay:true,
+        loop:true,
+        height:600,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    });
+    
+});
