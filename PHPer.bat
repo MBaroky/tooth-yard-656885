a63d3660426697e03f412ad4ed8a077d26aeef77
MBaroky <m.baroky@gmail.com>
2023-09-20 17:45:23 +0200

    added 2 more pages

--- PHPer.bat
+++ PHPer.bat
@@ -5,7 +5,7 @@ for %%i in (*.php) do if not "%%i"=="form.php" del /q "%%i"
 for %%i in (*.html) do if not "%%i"=="form.html" ren "%%i" *.php
 ::ren *.html *.php
 @echo off
-for %%a in (index.php, contact.php) do (
+for %%a in (index.php, contact.php, services.php, teeth.php) do (
    :: multi files with comma , seperator
    echo processing "%%a"
    type "%%a"|findrepl "end of header" /E:"start of footer" /O:+1:-1>"%%a.tmp"
