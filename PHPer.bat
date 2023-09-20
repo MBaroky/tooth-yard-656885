1c1a6d640fa45ea49bf6231dfd49eba8f47698a2
MBaroky <m.baroky@gmail.com>
2023-09-20 17:24:51 +0200

    PHPed files

---
+++ PHPer.bat
@@ -0,0 +1,24 @@
+set arg1=%1
+cd %arg1%
+::del contact.php, index.php, header.php, footer.php, tires.php, wheels.php, batteries.php
+for %%i in (*.php) do if not "%%i"=="form.php" del /q "%%i"
+for %%i in (*.html) do if not "%%i"=="form.html" ren "%%i" *.php
+::ren *.html *.php
+@echo off
+for %%a in (index.php, contact.php) do (
+   :: multi files with comma , seperator
+   echo processing "%%a"
+   type "%%a"|findrepl "end of header" /E:"start of footer" /O:+1:-1>"%%a.tmp"
+   move "%%a.tmp" "%%a" >nul
+)
+for %%a in (header.php) do (
+   echo processing "%%a"
+   type "%%a"|findrepl "<!DOCTYPE html>" /E:"end of header" >"%%a.tmp"
+   move "%%a.tmp" "%%a" >nul
+)
+for %%a in (footer.php) do (
+   echo processing "%%a"
+   type "%%a"|findrepl "start of footer" /E:"</html" >"%%a.tmp"
+   move "%%a.tmp" "%%a" >nul
+)
+:: cd en [uncomment then copy the above for second language]
---
+++ index.php
@@ -0,0 +1,73 @@
+<?php include 'header.php'; ?>
+
+    <header class="masthead" style="height: auto;padding: 0;min-height: 0 !important;">
+        <div class="container-fluid my-auto" style="padding: 0;">
+            <div class="row g-0 row-cols-1 row-cols-md-2">
+                <div class="col text-center pb-5" data-aos="fade-right" data-aos-once="true" style="padding-top: 100px;">
+                    <h1 class="text-center heading-font color-dark m-0" style="letter-spacing: 2px;font-weight: 400;font-size: 70px;">عيادة</h1>
+                    <h1 class="text-center heading-font color-main m-0" id="cover-company-name" style="font-size: 110px;font-weight: 700;font-family: Jenine;">توث يارد<br></h1>
+                    <h1 class="text-center heading-font color-dark m-0" id="cover-doctor-name" style="font-size: 55px;font-weight: 700;">د. نهى صدقى<br></h1>
+                    <h1 class="text-center heading-font color-main m-0" id="cover-slogan" style="font-size: 55px;font-weight: 100;">أستعيد بسمتك<br></h1><a class="btn btn-primary btn-lg text-center px-5 mt-3 bg-main" role="button" style="margin-right: auto;margin-left: auto;display: inline-block;font-family: Almarai, sans-serif;">احجز الان</a>
+                </div>
+                <div class="col" data-aos="fade-left" data-aos-once="true" style="max-height: 750px;">
+                    <div class="simple-slider">
+                        <div class="swiper">
+                            <div class="swiper-wrapper" style="height: auto;">
+                                <div class="swiper-slide" style="background: url(&quot;assets/img/8980c5_aaeebe58337a4a3aa28b8fc5806ad8f4mv2.png?h=9731367317187ec24719572665e48173&quot;) left / cover no-repeat;"></div>
+                                <div class="swiper-slide" style="background: url(&quot;assets/img/8980c5_00d583383faa4e74a73fe2168bc0a1ffmv2.png?h=1af17852015b4aad3532325336ddfbc1&quot;) left / cover no-repeat;"></div>
+                            </div>
+                            <div class="swiper-button-prev swiper-button"></div>
+                            <div class="swiper-button-next swiper-button"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section id="about" class="bg-primary" style="background: url(&quot;assets/img/8980c5_22d172895dea424a97686dddbd60a469mv2.jpg?h=af1a23db34806e5f5d4ab36c595bbfa9&quot;) center / cover no-repeat;padding-bottom: 0;height: 600px;">
+        <div class="container p-5">
+            <div class="row"></div>
+        </div>
+    </section>
+    <section id="about-1" class="py-0 px-5" style="padding-bottom: 0;margin-top: -450px;">
+        <div class="container py-5 px-md-5" style="background: #E2F7EB;max-width: 800px;">
+            <div class="row">
+                <div class="col offset-lg-8 text-center mx-auto" data-aos="fade-left" data-aos-duration="700" data-aos-once="true">
+                    <h2 class="section-heading rtl color-main" style="font-family: Jenine;font-size: 60px;">عن العيادة :<br></h2>
+                    <ul class="rtl" style="text-align: right;font-family: Lenos;font-size: 20px;">
+                        <li>تأسست عيادة توث يارد للأسنان وتديرها الدكتورة نهى صدقى عضو مجلس الجمعية الأمريكية لطب الاسنان اكثر من 14 سنة خبرة فى مجال طب الفم والأسنان</li>
+                        <li>تقدم عيادة توث يارد للأسنان مجموعة من امهر الاطباء المتخصصين في مجال طب الأسنان وخدمات تجميل الاسنان وطب اسنان الاطفال باحدث التقنيات الحديثة لعلاج أكثر راحة ودقة.</li>
+                        <li>يوجد في عيادة توث يارد للأسنان مواعيد خاصة بالسيدات.<br></li>
+                        <li>متوفر مواعيد صباحية ومسائية لتناسب جميع أفراد الأسرة.<br></li>
+                        <li>يوجد مكان مخصص لاستقبال الأطفال وذلك لراحة الأم.<br></li>
+                    </ul>
+                    <p class="color-main">متاح التقسيط بدون فوائد عن طريق<br></p>
+                    <div class="row">
+                        <div class="col"><img data-aos="zoom-in" data-aos-once="true" class="mx-2" src="assets/img/Cib_Logo_svg.png?h=c1144a92f58ea18493f5989a10b33024" style="width: 130px;"><img data-aos="zoom-in" data-aos-once="true" class="mx-2" src="assets/img/unnamed2.png?h=30ccc02f251207e9ac5dde8ea5df4b28" style="width: 70px;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="services" class="p-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading rtl heading-font fw-bold color-dark" style="font-size: 50px;">صور العيادة<br></h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="photo-gallery py-5 bg-main">
+        <div class="container">
+            <div class="row gx-2 gy-2 row-cols-2 row-cols-md-3 row-cols-lg-5 photos" data-bss-baguettebox="">
+                <div class="col item"><a href="assets/img/fullsize/home-1.webp?h=ada30391a2aa473e3f6230e4a36f1616"><img class="img-fluid" src="assets/img/thumbnails/thumb-1.jpeg?h=00079fdf24f74258b0cea45c6f711c5a"></a></div>
+                <div class="col item"><a href="assets/img/fullsize/home-2.webp?h=8cd690f8b80ddaa62761eb280178b51e"><img class="img-fluid" src="assets/img/thumbnails/thumb-2.jpeg?h=9208a0331d5b3fc83590883752316fbf"></a></div>
+                <div class="col item"><a href="assets/img/fullsize/home-3.webp?h=ab8062a4ef2a4600a70f271aec01e2de"><img class="img-fluid" src="assets/img/thumbnails/thumb-3.jpeg?h=cbdeb4c58d020db9c3d01772a3d9563a"></a></div>
+                <div class="col item"><a href="assets/img/fullsize/home-4.webp?h=622cf804f066c13af58cb5f15fabd1c4"><img class="img-fluid" src="assets/img/thumbnails/thumb-4.jpeg?h=15247de97310d25c73ac5c7ea1e013d2"></a></div>
+                <div class="col item"><a href="assets/img/fullsize/home-5.webp?h=622cf804f066c13af58cb5f15fabd1c4"><img class="img-fluid" src="assets/img/thumbnails/thumb-5.jpeg?h=bfef493f2a15c6693fb7803ca54cb2c2"></a></div>
+            </div>
+        </div>
+    </section>
+    <!--end of content-->
+<?php include 'footer.php'; ?>
