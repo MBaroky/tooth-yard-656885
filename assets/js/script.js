dad7a77b096e1ecfe3982acd2b365487d595d73a
Mohamed Baroky <m.baroky@ype-dc01.dom>
2023-10-04 16:46:23 +0200

    basic changes

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+const successMessage = document.getElementById("sentMailSuc");
+  successMessage ? successMessage.scrollIntoView()
+  : console.log("def");
+
+});
---
+++ assets/js/templating.js
@@ -0,0 +1,34 @@
+//     load template files
+HTMLElement.prototype.removeChildren = function (cssSelector) {
+  // Loop through the array of indices
+  let indices = this.querySelectorAll(cssSelector);
+  for (let i = 0; i < indices.length; i++) {
+    // Remove the child element at that index
+    this.removeChild(indices[i]);
+  }
+};
+// function removeChildren(cssSelector, parentNode) {
+//   var elements = parentNode.querySelectorAll(cssSelector);
+//   let fragment = document.createDocumentFragment();
+//   fragment.textContent = " ";
+//   fragment.firstChild.replaceWith(...elements);
+// }
+
+function loadHTML(names) {
+  names.forEach((name, i) => {
+    let temp = document.createElement("div");
+    fetch(`${name}.html`)
+      .then(response => response.text())
+      .then(text => {
+        temp.innerHTML = text;
+
+        i == 0
+          ? document.body.prepend(temp.querySelectorAll("nav")[0])
+          : document.body.append(temp.querySelectorAll("footer")[0]);
+      });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  loadHTML(["header", "footer"]);
+});
