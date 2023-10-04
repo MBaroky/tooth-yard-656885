6e895a1baace082182dd9f96760a8e17dfd1ac82
Mohamed Baroky <m.baroky@ype-dc01.dom>
2023-10-04 10:08:38 +0200

    initial commit

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-const successMessage = document.getElementById("sentMailSuc");
-  successMessage ? successMessage.scrollIntoView()
-  : console.log("def");
-});
